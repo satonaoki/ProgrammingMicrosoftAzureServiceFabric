feabf86f1b7c5695980a875e29af14a10b8e5165
satonaoki <satonaoki@gmail.com>
2017-01-16 05:54:49 +0900

    ch5 added

---
+++ Chapter5/ConsoleRedirectTestApplication/ConsoleRedirect/ConsoleRedirect.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace ConsoleRedirect
+{
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class ConsoleRedirect : StatelessService
+    {
+        public ConsoleRedirect(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[0];
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following sample code with your own logic 
+            //       or remove this RunAsync override if it's not needed in your service.
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var dataPackage = this.Context.CodePackageActivationContext.GetDataPackageObject("TestData");
+                var text = File.ReadAllText(Path.Combine(dataPackage.Path, "data.txt"));
+                ServiceEventSource.Current.ServiceMessage(this, text);
+                await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+            }
+        }
+    }
+}
