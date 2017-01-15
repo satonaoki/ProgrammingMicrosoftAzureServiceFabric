951047f1b1b9a44fdc6727a435b7b78b3f23f3cc
satonaoki <satonaoki@gmail.com>
2017-01-15 19:19:30 +0900

    v2 added

---
+++ Chapter2/CalculatorApplication2/CalculatorClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CalculatorService;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using System.Threading;
+
+namespace CalculatorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ICalculatorService calculatorClient =
+                ServiceProxy.Create<ICalculatorService>(
+                new Uri("fabric:/CalculatorApplication2/CalculatorService"));
+                var result = calculatorClient.Add(1, 2).Result;
+                Console.WriteLine(result);
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
---
+++ Chapter2/CalculatorApplication2/CalculatorService/ICalculatorService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System.Threading.Tasks;
+
+namespace CalculatorService
+{
+    public interface ICalculatorService : IService
+    {
+        Task<string> Add(int a, int b);
+        Task<string> Subtract(int a, int b);
+    }
+}
