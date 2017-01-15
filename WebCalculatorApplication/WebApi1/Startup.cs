f7c0d4150aa98d2f1b2c675b07d83a387c3b7b39
satonaoki <satonaoki@gmail.com>
2017-01-15 20:31:57 +0900

    v3 added

---
+++ WebCalculatorApplication/WebApi1/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace WebApi1
+{
+    public static class Startup
+    {
+        // このコードは Web API を構成します。スタートアップ クラスは、
+        // WebApp.Start メソッドの型パラメーターとして指定されます。
+        public static void ConfigureApp(IAppBuilder appBuilder)
+        {
+            // セルフ ホストの Web API を構成します。
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
---
+++ WebCalculatorApplication/WebCalculatorService/WebCalculatorService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace WebCalculatorService
+{
+    /// <summary>
+    /// Service Fabric ランタイムによって、このクラスのインスタンスがサービス インスタンスごとに作成されます。
+    /// </summary>
+    internal sealed class WebCalculatorService : StatelessService
+    {
+        public WebCalculatorService(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// このサービス レプリカがクライアントやユーザーの要求を処理するために、リスナー (TCP、HTTP など) を作成するようオーバーライドします (省略可能)。
+        /// </summary>
+        /// <returns>リスナーのコレクション。</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[0];
+        }
+
+        /// <summary>
+        /// これは、サービス インスタンスのメイン エントリ ポイントです。
+        /// </summary>
+        /// <param name="cancellationToken">Service Fabric がこのサービス インスタンスをシャットダウンする必要が生じると、キャンセルされます。</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: 次のサンプル コードを独自のロジックに置き換えるか、
+            //       サービスで不要な場合にはこの RunAsync オーバーライドを削除します。
+
+            long iterations = 0;
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
